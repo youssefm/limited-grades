f663fbcefa3e8a9fa0d95fcbf20aebe068990a0c
Youssef Moussaoui <yms@outlook.com>
2021-09-27 17:54:27 -0700

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/mid",
+        permanent: false,
+      },
+      {
+        source: "/:expansion",
+        destination: "/:expansion/all",
+        permanent: true,
+      },
+    ];
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/dist/shared/lib/router/router";
+import React from "react";
+import Layout from "../components/Layout";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
-export default MyApp
