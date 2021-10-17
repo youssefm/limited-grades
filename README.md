33c7ea855ece0d52237cea33f3e09890eb31fbf3
Youssef Moussaoui <yms@outlook.com>
2021-10-17 15:22:41 -0700

    Add hyperlink to metric definition

--- README.md
+++ README.md
@@ -6,18 +6,6 @@ First, run the development server:
 
 ```bash
 npm run dev
-# or
-yarn dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
-
-## TODO
--- pages/[set]/[deck].tsx
+++ pages/[set]/[deck].tsx
@@ -83,11 +83,14 @@ const TierList = ({
             The table below uses{" "}
             <a href="https://www.17lands.com/card_ratings">17Lands</a> Premier
             Draft data to assign letter grades to cards. It infers a normal
-            distribution from the &quot;Games in Hand Win Rate&quot; statistic
-            and uses that distribution to assign a grade to each card. For
-            example, a card with an GIH winrate statistic that is one standard
-            deviation higher than the mean would get a B+. Cards with fewer than
-            400 games are not included.
+            distribution from the{" "}
+            <a href="https://www.17lands.com/metrics_definitions">
+              Games in Hand Win Rate
+            </a>{" "}
+            statistic and uses that distribution to assign a grade to each card.
+            For example, a card with an GIH winrate statistic that is one
+            standard deviation higher than the mean would get a B+. Cards with
+            fewer than 400 games are not included.
           </p>
         </Col>
       </Row>
