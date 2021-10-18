de52e5e43c69b2401f8e3f5f67a93393f58fb48f
Youssef Moussaoui <yms@outlook.com>
2021-10-18 16:40:56 -0700

    content update

--- pages/[set]/[deck].tsx
+++ pages/[set]/[deck].tsx
@@ -88,8 +88,8 @@ const TierList = ({
               Games in Hand Win Rate
             </a>{" "}
             statistic and uses that distribution to assign a grade to each card.
-            For example, a card with an GIH winrate statistic that is one
-            standard deviation higher than the mean would get a B+. Cards with
+            For example, a card with a GIH winrate statistic that is one
+            standard deviation higher than the mean would get a B. Cards with
             fewer than 400 games are not included.
           </p>
         </Col>
