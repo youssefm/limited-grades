309e34bc5a85adeaaa45853380a04b2b245aa6b2
Youssef Moussaoui <yms@outlook.com>
2022-01-14 17:01:34 -0800

    Throw on calls to cache.connect if cache is not enabled

--- lib/cache.ts
+++ lib/cache.ts
@@ -8,6 +8,10 @@ import {
 export const IS_ENABLED = process.env.REDIS_URL !== undefined;
 
 export async function connect() {
+  if (!IS_ENABLED) {
+    throw "Cannot connect to cache when it is not enabled";
+  }
+
   const redisClient = createClient({
     url: process.env.REDIS_URL,
     socket: {
