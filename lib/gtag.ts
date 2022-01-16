3bc41726befe5787206aa09da66d07c6049d59f9
Youssef Moussaoui <yms@outlook.com>
2022-01-15 19:20:05 -0800

    Add support for Google analytics
    Former-commit-id: ca71f6f9c0747e8f527ad107629b5c4282e04833

---
+++ lib/gtag.ts
@@ -0,0 +1,27 @@
+export const GA_TRACKING_ID = process.env.GA_TRACKING_ID;
+export const GA_TRACKING_ENABLED = GA_TRACKING_ID !== undefined;
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url: string) => {
+  if (GA_TRACKING_ENABLED) {
+    window.gtag("config", GA_TRACKING_ID!, {
+      page_path: url,
+    });
+  }
+};
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = (
+  action: string,
+  category: string,
+  label: string,
+  value: number
+) => {
+  if (GA_TRACKING_ENABLED) {
+    window.gtag("event", action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    });
+  }
+};
