7d1f865777efcc5d818c5f05c750958382434828
Youssef Moussaoui <yms@outlook.com>
2022-01-17 09:02:43 -0800

    Cut out bootstrap

--- components/CardDetailModal.tsx
+++ components/CardDetailModal.tsx
@@ -1,5 +1,4 @@
 import { sortBy } from "lodash";
-import { Col, Modal, Row, Table } from "react-bootstrap";
 
 import { COLUMN_ICONS, DECK_COLORS, DECK_LABELS } from "lib/constants";
 import { Card, Deck } from "lib/types";
@@ -16,70 +15,72 @@ const CardDetailModal = (props: Props) => {
     return null;
   }
 
-  return (
-    <Modal
-      show={card}
-      onHide={handleClose}
-      size="lg"
-      centered
-      animation={false}
-    >
-      <Modal.Header className="text-center" closeButton>
-        <Modal.Title>{card.name}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Row>
-          <Col md="auto" className="align-self-center">
-            <img src={card.cardUrl} alt={card.name} width="240" height="340" />
-          </Col>
-          <Col>
-            <Table>
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>Win Rate</th>
-                  <th>Grade</th>
-                </tr>
-              </thead>
-              <tbody>
-                {sortBy(
-                  Object.entries(card.stats),
-                  ([deck, stats]) => -stats.gameCount
-                ).map(([deck, stats]) => {
-                  const deckColors = DECK_COLORS[deck as Deck];
-                  return (
-                    <tr key={deck}>
-                      <th>
-                        {deckColors.length > 0 ? (
-                          <>
-                            {deckColors.map((column) => (
-                              <i
-                                key={column}
-                                className={COLUMN_ICONS[column]}
-                              />
-                            ))}
-                          </>
-                        ) : (
-                          DECK_LABELS[deck as Deck]
-                        )}
-                      </th>
-                      <td>
-                        {Number(stats.winrate).toLocaleString(undefined, {
-                          style: "percent",
-                          minimumFractionDigits: 1,
-                        })}
-                      </td>
-                      <td>{stats.grade}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </Table>
-          </Col>
-        </Row>
-      </Modal.Body>
-    </Modal>
-  );
+  return null;
+  // TODO: Re-enable modal
+  // return (
+  //   <Modal
+  //     show={card}
+  //     onHide={handleClose}
+  //     size="lg"
+  //     centered
+  //     animation={false}
+  //   >
+  //     <Modal.Header className="text-center" closeButton>
+  //       <Modal.Title>{card.name}</Modal.Title>
+  //     </Modal.Header>
+  //     <Modal.Body>
+  //       <Row>
+  //         <Col md="auto" className="align-self-center">
+  //           <img src={card.cardUrl} alt={card.name} width="240" height="340" />
+  //         </Col>
+  //         <Col>
+  //           <Table>
+  //             <thead>
+  //               <tr>
+  //                 <th></th>
+  //                 <th>Win Rate</th>
+  //                 <th>Grade</th>
+  //               </tr>
+  //             </thead>
+  //             <tbody>
+  //               {sortBy(
+  //                 Object.entries(card.stats),
+  //                 ([deck, stats]) => -stats.gameCount
+  //               ).map(([deck, stats]) => {
+  //                 const deckColors = DECK_COLORS[deck as Deck];
+  //                 return (
+  //                   <tr key={deck}>
+  //                     <th>
+  //                       {deckColors.length > 0 ? (
+  //                         <>
+  //                           {deckColors.map((column) => (
+  //                             <i
+  //                               key={column}
+  //                               className={COLUMN_ICONS[column]}
+  //                             />
+  //                           ))}
+  //                         </>
+  //                       ) : (
+  //                         DECK_LABELS[deck as Deck]
+  //                       )}
+  //                     </th>
+  //                     <td>
+  //                       {Number(stats.winrate).toLocaleString(undefined, {
+  //                         style: "percent",
+  //                         minimumFractionDigits: 1,
+  //                       })}
+  //                     </td>
+  //                     <td>{stats.grade}</td>
+  //                   </tr>
+  //                 );
+  //               })}
+  //             </tbody>
+  //           </Table>
+  //         </Col>
+  //       </Row>
+  //     </Modal.Body>
+  //   </Modal>
+  // );
 };
 
 export default CardDetailModal;
--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -1,33 +1,27 @@
-import { Col, Row } from "react-bootstrap";
-
+// TODO: Move PageHeader to about this page tooltip
 const PageHeader = () => (
-  <Row className="justify-content-center">
-    <Col xl="6">
-      <p>
-        The table below uses{" "}
-        <a
-          href="https://www.17lands.com/card_ratings"
-          target="_blank"
-          rel="noreferrer"
-        >
-          17Lands
-        </a>{" "}
-        Premier Draft data to assign letter grades to cards. It infers a normal
-        distribution from the{" "}
-        <a
-          href="https://www.17lands.com/metrics_definitions#:~:text=Games%20in%20Hand%20Win%20Rate%20(GIH%20WR)"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Games in Hand Win Rate
-        </a>{" "}
-        statistic and uses that distribution to assign a grade to each card. For
-        example, a card with a win rate that is one standard deviation higher
-        than the mean would get a B. Cards drawn fewer than 200 times are not
-        included.
-      </p>
-    </Col>
-  </Row>
+  <p>
+    The table below uses{" "}
+    <a
+      href="https://www.17lands.com/card_ratings"
+      target="_blank"
+      rel="noreferrer"
+    >
+      17Lands
+    </a>{" "}
+    Premier Draft data to assign letter grades to cards. It infers a normal
+    distribution from the{" "}
+    <a
+      href="https://www.17lands.com/metrics_definitions#:~:text=Games%20in%20Hand%20Win%20Rate%20(GIH%20WR)"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Games in Hand Win Rate
+    </a>{" "}
+    statistic and uses that distribution to assign a grade to each card. For
+    example, a card with a win rate that is one standard deviation higher than
+    the mean would get a B. Cards drawn fewer than 200 times are not included.
+  </p>
 );
 
 export default PageHeader;
