9d9a67bd658f634623fbb4abad67cbfeffbb14e6
Youssef Moussaoui <yms@outlook.com>
2022-01-18 14:08:02 -0800

    Create an alias for card-text
    Former-commit-id: cba46eb50ea57b2afc8036fe60da61358a29c214

--- components/CardView.tsx
+++ components/CardView.tsx
@@ -23,33 +23,29 @@ const CardView: FC<Props> = (props) => {
   const tooltipWidthClass = card.cardBackUrl ? `w-[480px]` : `w-[240px]`;
 
   return (
-    <div className="mb-1 last:mb-0">
-      <LazyTippy
-        content={
-          <div className={`flex ${tooltipWidthClass}`}>
-            <img src={card.cardUrl} alt={card.name} width="240" height="340" />
-            {card.cardBackUrl && (
-              <img
-                src={card.cardBackUrl}
-                alt={card.name}
-                width="240"
-                height="340"
-              />
-            )}
-          </div>
-        }
-        placement="bottom-start"
-      >
-        <div
-          onClick={onClick}
-          className={`w-full bg-white py-0.5 px-2 rounded border-l-[3px] cursor-pointer text-sm hover:text-zinc-500 ${
-            BORDER_COLORS[card.rarity]
-          }`}
-        >
-          {card.name}
+    <LazyTippy
+      content={
+        <div className={`flex ${tooltipWidthClass}`}>
+          <img src={card.cardUrl} alt={card.name} width="240" height="340" />
+          {card.cardBackUrl && (
+            <img
+              src={card.cardBackUrl}
+              alt={card.name}
+              width="240"
+              height="340"
+            />
+          )}
         </div>
-      </LazyTippy>
-    </div>
+      }
+      placement="bottom-start"
+    >
+      <div
+        onClick={onClick}
+        className={`card-bubble ${BORDER_COLORS[card.rarity]}`}
+      >
+        {card.name}
+      </div>
+    </LazyTippy>
   );
 };
 
