d587593d9eeede22dd731b1818df4634d80009ba
Youssef Moussaoui <yms@outlook.com>
2022-01-19 12:10:53 -0800

    Set close button as initial focus for modals

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,32 +1,31 @@
 import { Dialog } from "@headlessui/react";
-import { FC } from "react";
+import { FC, useRef } from "react";
 import { IoClose } from "react-icons/io5";
 
 interface Props {
-  title?: string;
+  title: string;
   onClose: () => void;
 }
 
 const Modal: FC<Props> = (props) => {
   const { title, onClose, children } = props;
+  const completeButtonRef = useRef(null);
 
   return (
     <Dialog
       open
       onClose={onClose}
       className="fixed inset-0 flex justify-center items-center"
+      initialFocus={completeButtonRef}
     >
       <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-50 z-10" />
       <div className="bg-white rounded-lg shadow-xl z-20 sm:max-w-3xl sm:w-full">
-        {title && (
-          <Dialog.Title className="p-4 border-b-[1px] flex w-full items-center">
-            <span className="flex-grow text-2xl font-medium">{title}</span>
-            <IoClose
-              onClick={onClose}
-              className="text-2xl hover:text-zinc-500 hover:cursor-pointer"
-            />
-          </Dialog.Title>
-        )}
+        <Dialog.Title className="p-4 border-b-[1px] flex w-full items-center">
+          <span className="flex-grow text-2xl font-medium">{title}</span>
+          <button onClick={onClose} ref={completeButtonRef}>
+            <IoClose className="text-2xl hover:text-zinc-500" />
+          </button>
+        </Dialog.Title>
         <div className="p-4">{children}</div>
       </div>
     </Dialog>
