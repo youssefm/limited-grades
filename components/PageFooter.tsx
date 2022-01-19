c3f4409f9d7016235079be6ff48a8b990ec8dd8c
Youssef Moussaoui <yms@outlook.com>
2022-01-19 14:00:55 -0800

    Factor out a component for external links

--- components/PageFooter.tsx
+++ components/PageFooter.tsx
@@ -1,46 +1,22 @@
+import LinkOut from "components/LinkOut";
+
 interface Props {
   lastUpdatedAtTicks: number;
 }
 
 const PageFooter = ({ lastUpdatedAtTicks }: Props) => (
   <div className="mt-32 border-t-[1px] border-zinc-400 pt-1">
-    Designed by{" "}
-    <a href="https://github.com/BodyCopy" target="_blank" rel="noreferrer">
-      BodyCopy
-    </a>{" "}
+    Designed by <LinkOut url="https://github.com/BodyCopy">BodyCopy</LinkOut>{" "}
     and developed by{" "}
-    <a href="https://github.com/youssefm" target="_blank" rel="noreferrer">
-      youssefm
-    </a>{" "}
-    using{" "}
-    <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-      Next.js
-    </a>
+    <LinkOut url="https://github.com/youssefm">youssefm</LinkOut> using{" "}
+    <LinkOut url="https://nextjs.org/">Next.js</LinkOut>
     <br />
     Made possible thanks to{" "}
-    <a href="https://www.17lands.com" target="_blank" rel="noreferrer">
-      17Lands
-    </a>
-    ,{" "}
-    <a href="https://scryfall.com" target="_blank" rel="noreferrer">
-      Scryfall
-    </a>
-    ,{" "}
-    <a
-      href="https://www.flaticon.com/authors/freepik"
-      target="_blank"
-      rel="noreferrer"
-    >
-      Freepik
-    </a>
-    ,{" "}
-    <a href="https://keyrune.andrewgioia.com" target="_blank" rel="noreferrer">
-      Keyrune
-    </a>
-    , and{" "}
-    <a href="https://mana.andrewgioia.com" target="_blank" rel="noreferrer">
-      Mana
-    </a>
+    <LinkOut url="https://www.17lands.com">17Lands</LinkOut>,{" "}
+    <LinkOut url="https://scryfall.com">Scryfall</LinkOut>,{" "}
+    <LinkOut url="https://www.flaticon.com/authors/freepik">Freepik</LinkOut>,{" "}
+    <LinkOut url="https://keyrune.andrewgioia.com">Keyrune</LinkOut>, and{" "}
+    <LinkOut url="https://mana.andrewgioia.com">Mana</LinkOut>
     <br />
     <em>Last updated at: {new Date(lastUpdatedAtTicks).toLocaleString()}</em>
   </div>
