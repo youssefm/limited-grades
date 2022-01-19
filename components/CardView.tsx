ac31faf263bacf226d9aae7b65d7f69078e156c8
Youssef Moussaoui <yms@outlook.com>
2022-01-19 10:09:27 -0800

    Disable hover on touch devices in a better way

--- components/CardView.tsx
+++ components/CardView.tsx
@@ -1,8 +1,8 @@
 import { FC } from "react";
 
-import CardBubble from "components/CardBubble";
 import LazyTippy from "components/LazyTippy";
 import { Card } from "lib/types";
+import CardBubble from "components/CardBubble";
 
 interface Props {
   card: Card;
@@ -14,26 +14,33 @@ const CardView: FC<Props> = (props) => {
 
   const tooltipWidthClass = card.cardBackUrl ? `w-[480px]` : `w-[240px]`;
 
-  return (
-    <LazyTippy
-      content={
-        <div className={`flex ${tooltipWidthClass}`}>
-          <img src={card.cardUrl} alt={card.name} width="240" height="340" />
-          {card.cardBackUrl && (
-            <img
-              src={card.cardBackUrl}
-              alt={card.name}
-              width="240"
-              height="340"
-            />
-          )}
-        </div>
-      }
-      placement="bottom-start"
-    >
-      <CardBubble card={card} onClick={onClick} />
-    </LazyTippy>
-  );
+  if (
+    typeof window !== "undefined" &&
+    window.matchMedia("(hover: hover)").matches
+  ) {
+    return (
+      <LazyTippy
+        content={
+          <div className={`flex ${tooltipWidthClass}`}>
+            <img src={card.cardUrl} alt={card.name} width="240" height="340" />
+            {card.cardBackUrl && (
+              <img
+                src={card.cardBackUrl}
+                alt={card.name}
+                width="240"
+                height="340"
+              />
+            )}
+          </div>
+        }
+        placement="bottom-start"
+      >
+        <CardBubble card={card} onClick={onClick} />
+      </LazyTippy>
+    );
+  }
+
+  return <CardBubble card={card} onClick={onClick} />;
 };
 
 export default CardView;
