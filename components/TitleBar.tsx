fa323b5e469fb469b6f9bb6ac7afb9bff84745f4
Youssef Moussaoui <yms@outlook.com>
2022-01-19 15:45:43 -0800

    Factor out about project modal
    Former-commit-id: 01ad42fac9f05e9ef23be1c9ae955b692c5532b8

--- components/TitleBar.tsx
+++ components/TitleBar.tsx
@@ -2,8 +2,7 @@ import Image from "next/image";
 import { useState } from "react";
 import { FaInfoCircle } from "react-icons/fa";
 
-import LinkOut from "components/LinkOut";
-import Modal from "components/Modal";
+import AboutProjectModal from "components/AboutProjectModal";
 
 const TitleBar = () => {
   const [showInfoModal, setShowInfoModal] = useState(false);
@@ -31,40 +30,7 @@ const TitleBar = () => {
       </button>
 
       {showInfoModal && (
-        <Modal
-          title="About this project"
-          onClose={() => setShowInfoModal(false)}
-        >
-          <div className="max-w-[80ch]">
-            <p className="mb-4">
-              This page uses{" "}
-              <LinkOut url="https://www.17lands.com/card_ratings">
-                17Lands
-              </LinkOut>{" "}
-              Premier Draft data to assign letter grades to cards. It infers a
-              normal distribution from the{" "}
-              <LinkOut url="https://www.17lands.com/metrics_definitions#:~:text=Games%20in%20Hand%20Win%20Rate%20(GIH%20WR)">
-                Games in Hand Win Rate
-              </LinkOut>{" "}
-              statistic and uses that distribution to assign a grade to each
-              card. For example, a card with a win rate that is one standard
-              deviation higher than the mean would get a B. Cards drawn fewer
-              than 200 times are not included.
-            </p>
-            <p className="mb-4">
-              You can hover over cards to see card previews and click on cards
-              to get a breakdown of how well the card does in different
-              archetypes.
-            </p>
-            <p>
-              The source code for this site can be found at{" "}
-              <LinkOut url="https://github.com/youssefm/limited-grades">
-                https://github.com/youssefm/limited-grades
-              </LinkOut>
-              .
-            </p>
-          </div>
-        </Modal>
+        <AboutProjectModal onClose={() => setShowInfoModal(false)} />
       )}
     </div>
   );
