f0047800234ef2b915c4fdf2fa4ee6216158f910
Youssef Moussaoui <yms@outlook.com>
2022-01-21 14:39:44 -0800

    Refactor CardTypeFilter and RarityFilter in terms of IconFilterGroup

--- components/CardTypeFilter.tsx
+++ components/CardTypeFilter.tsx
@@ -1,28 +1,29 @@
 import { FC } from "react";
 
 import FilterLabel from "components/FilterLabel";
+import IconFilterGroup from "components/IconFilterGroup";
 import { CardType } from "lib/types";
 
 const FILTERS = [
   {
     label: "creatures",
     values: [CardType.CREATURE],
-    icon: "ms ms-2x ms-creature",
+    icon: "ms ms-2x ms-creature w-[1.28571429em] text-center",
   },
   {
     label: "instants and sorceries",
     values: [CardType.INSTANT, CardType.SORCERY],
-    icon: "ms ms-2x ms-instant",
+    icon: "ms ms-2x ms-instant w-[1.28571429em] text-center",
   },
   {
     label: "artifacts, enchantments, and planeswalkers",
     values: [CardType.ARTIFACT, CardType.ENCHANTMENT, CardType.PLANESWALKER],
-    icon: "ms ms-2x ms-enchantment",
+    icon: "ms ms-2x ms-enchantment w-[1.28571429em] text-center",
   },
   {
     label: "lands",
     values: [CardType.LAND],
-    icon: "ms ms-2x ms-land",
+    icon: "ms ms-2x ms-land w-[1.28571429em] text-center",
   },
 ];
 
@@ -34,35 +35,7 @@ interface Props {
 const CardTypeFilter: FC<Props> = ({ values, setValues }) => (
   <div>
     <FilterLabel>Type</FilterLabel>
-    <div>
-      {FILTERS.map((filter) => {
-        const checked = filter.values.every((cardType) => values.has(cardType));
-        return (
-          <label key={filter.label}>
-            <input
-              type="checkbox"
-              checked={checked}
-              onChange={() => {
-                const newValues = new Set(values);
-                for (const cardType of filter.values) {
-                  if (newValues.has(cardType)) {
-                    newValues.delete(cardType);
-                  } else {
-                    newValues.add(cardType);
-                  }
-                }
-                setValues(newValues);
-              }}
-              className="peer hidden"
-            />
-            <i
-              title={checked ? `Hide ${filter.label}` : `Show ${filter.label}`}
-              className={`${filter.icon} cursor-pointer opacity-30 peer-checked:opacity-90 w-[1.28571429em] text-center`}
-            />
-          </label>
-        );
-      })}
-    </div>
+    <IconFilterGroup values={values} setValues={setValues} filters={FILTERS} />
   </div>
 );
 
