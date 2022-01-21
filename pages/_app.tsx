bdae2ab1c4f643db9e9901868ad9a2f05d83dda2
Youssef Moussaoui <yms@outlook.com>
2022-01-21 09:23:10 -0800

    Minify analytics loading code
    Former-commit-id: 46d6c970b85dba308feb5d8f11397f3c43374a18

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,14 +33,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
             id="gtag-init"
             strategy="afterInteractive"
             dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
+              __html: `function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","${GA_TRACKING_ID}",{page_path:window.location.pathname});`,
             }}
           />
         </>
