4e4979a531e7eff09516d30bd661fc83223726a0
Youssef Moussaoui <yms@outlook.com>
2022-01-22 15:51:39 -0800

    Install eslint plugin for Tailwind
    Former-commit-id: d2701439e12f1e453d593905590e2b7ef504380a

--- components/IconFilterGroup.tsx
+++ components/IconFilterGroup.tsx
@@ -36,10 +36,9 @@ const IconFilterGroup = <T extends unknown>({
         <div
           key={label}
           className={clsx(
-            "w-[46px] h-[36px]",
-            "border border-zinc-300 hover:border-zinc-500",
-            "ml-[-1px] first:ml-0 focus:z-10 hover:z-20",
-            "first:rounded-l last:rounded-r overflow-hidden"
+            "overflow-hidden hover:z-20 focus:z-10 ml-[-1px] first:ml-0",
+            "w-[46px] h-[36px] last:rounded-r first:rounded-l",
+            "border border-zinc-300 hover:border-zinc-500"
           )}
           tabIndex={0}
           role="checkbox"
@@ -56,11 +55,11 @@ const IconFilterGroup = <T extends unknown>({
               type="checkbox"
               checked={checked}
               onChange={toggle}
-              className="hidden peer"
+              className="peer hidden"
             />
             <div
               className={clsx(
-                "flex items-center justify-center w-full h-full",
+                "flex justify-center items-center w-full h-full",
                 "bg-zinc-200 peer-checked:bg-zinc-50 hover:!bg-zinc-200",
                 "opacity-30 peer-checked:opacity-90"
               )}
