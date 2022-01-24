99fb1e56bc2f5dd06c326b865a86f5328953155b
Youssef Moussaoui <yms@outlook.com>
2022-01-23 19:59:55 -0800

    Persist dark mode value in local storage

--- components/common/DarkModeInitializer.tsx
+++ components/common/DarkModeInitializer.tsx
@@ -1,12 +1,28 @@
 import { minify } from "uglify-js";
 
+import {
+  DARK_MODE_CLASS,
+  LOCAL_STORAGE_DARK_VALUE,
+  LOCAL_STORAGE_THEME_KEY,
+} from "hooks/useDarkMode";
+
+// This script prevents the screen from flickering from light to dark when a user prefers dark mode
+// Thorough explanation here: https://blog.maximeheckel.com/posts/switching-off-the-lights-part-2-fixing-dark-mode-flashing-on-servered-rendered-website/
 const DarkModeInitializer = () => (
   <script
     // eslint-disable-next-line react/no-danger
     dangerouslySetInnerHTML={{
       __html: minify(`
-        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-          document.body.classList.add("dark")
+        let preferredTheme;
+        try {
+          preferredTheme = localStorage.getItem("${LOCAL_STORAGE_THEME_KEY}");
+        } catch (error) {}
+        if (
+          preferredTheme === "${LOCAL_STORAGE_DARK_VALUE}" ||
+          (!preferredTheme &&
+            window.matchMedia("(prefers-color-scheme: dark)").matches)
+        ) {
+          document.body.classList.add("${DARK_MODE_CLASS}");
         }
       `).code,
     }}
