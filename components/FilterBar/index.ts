5fbc6a93b21bdf389801ad690226dd174342baa5
Youssef Moussaoui <yms@outlook.com>
2022-01-23 20:59:24 -0800

    Create index files for modules

---
+++ components/FilterBar/index.ts
@@ -0,0 +1,4 @@
+export { default as CardTypeFilter } from "./CardTypeFilter";
+export { default as DeckSelector } from "./DeckSelector";
+export { default as RarityFilter } from "./RarityFilter";
+export { default as SetSelector } from "./SetSelector";
