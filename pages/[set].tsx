ad1efbcf36372e7c15768e8805b98ad28ed6fb8b
Youssef Moussaoui <yms@outlook.com>
2022-01-25 15:05:42 -0800

    Hide filters on smaller screens

--- pages/[set].tsx
+++ pages/[set].tsx
@@ -117,7 +117,7 @@ const Page = ({ set, cards, lastUpdatedAtTicks }: StaticProps) => {
             }}
           />
           <DeckSelector value={deck} onChange={setDeck} />
-          <div className="flex flex-wrap gap-2 justify-center lg:gap-4 lg:justify-start">
+          <div className="hidden gap-4 lg:flex">
             <RarityFilter
               set={set}
               values={visibleRarities}
