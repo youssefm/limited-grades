df14c7db81653313529019f2bd1e7c27c1973f2b
Youssef Moussaoui <yms@outlook.com>
2022-02-08 14:31:27 -0800

    Minor modal style updates
    Former-commit-id: 22c230a958c2c07bc2c4820d8822208b1033674b

--- components/common/Modal.tsx
+++ components/common/Modal.tsx
@@ -1,4 +1,5 @@
 import { Dialog } from "@headlessui/react";
+import clsx from "clsx";
 import { FC, useRef } from "react";
 import { IoClose } from "react-icons/io5";
 
@@ -20,8 +21,13 @@ const Modal: FC<Props> = ({ title, onClose, children }) => {
       initialFocus={completeButtonRef}
     >
       <Dialog.Overlay className="fixed inset-0 z-10 bg-black/50 backdrop-blur-xs" />
-      <div className="z-20 w-full max-w-3xl max-h-screen dark:text-neutral-100 bg-white dark:bg-neutral-800 rounded-lg shadow-xl">
-        <Dialog.Title className="flex items-center p-4 w-full text-2xl border-b">
+      <div
+        className={clsx(
+          "z-20 w-full max-w-3xl max-h-screen dark:text-neutral-100",
+          "bg-white dark:bg-neutral-800 rounded-lg border dark:border-black shadow-xl"
+        )}
+      >
+        <Dialog.Title className="flex items-center p-4 w-full text-2xl border-b border-neutral-200 dark:border-black">
           <span className="grow font-medium">{title}</span>
           <button onClick={onClose} type="button" ref={completeButtonRef}>
             <IoClose className={HOVER_CLASSES} />
