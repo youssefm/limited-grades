f170d26a73a91020bc7c99cda501d9b2041085e9
Youssef Moussaoui <yms@outlook.com>
2022-02-10 17:26:32 -0800

    Count all cards when forming the normal distribution

--- lib/cards.ts
+++ lib/cards.ts
@@ -96,11 +96,7 @@ const buildCardStore = async (set: MagicSet): Promise<CardStore> => {
   );
   for (const [index, deck] of ALL_DECKS.entries()) {
     let apiCards: ApiCard[] = apiCardStore[index];
-    apiCards = apiCards.filter(
-      (card) =>
-        card.ever_drawn_game_count >= MINIMUM_GAMES_DRAWN &&
-        card.ever_drawn_win_rate
-    );
+    apiCards = apiCards.filter((card) => card.ever_drawn_win_rate);
 
     if (apiCards.length <= 1) {
       continue;
@@ -110,6 +106,10 @@ const buildCardStore = async (set: MagicSet): Promise<CardStore> => {
     const normalDistribution = new NormalDistribution(
       mean(winrates),
       std(winrates)
+    );
+
+    apiCards = apiCards.filter(
+      (card) => card.ever_drawn_game_count >= MINIMUM_GAMES_DRAWN
     );
 
     for (const apiCard of apiCards) {
