fc27d570afb1b513316c9623f6d58070586d7785
Youssef Moussaoui <yms@outlook.com>
2022-02-18 11:47:26 -0800

    Add animation for banner being dismissed

--- components/PageHeader/Banner.tsx
+++ components/PageHeader/Banner.tsx
@@ -13,35 +13,35 @@ interface Props {
 const Banner: FC<Props> = ({ onReadMore }) => {
   const [isVisible, setIsVisible] = useState(true);
 
-  if (!isVisible) {
-    return null;
-  }
-
   return (
     <div
       className={clsx(
-        "flex items-center py-1.5 px-4 mb-2 bg-neutral-100 dark:bg-neutral-800 rounded-lg",
-        TRANSITION_CLASSES
+        "overflow-hidden mb-2",
+        { "max-h-15 sm:max-h-9": isVisible, "max-h-0": !isVisible },
+        "transition-max-h ease-[ease]"
       )}
     >
-      <ImInfo className="mr-2 min-w-[1rem]" />
-      <div className="grow">
-        Grades below are based on{" "}
-        <LinkOut url="https://www.17lands.com/">17Lands</LinkOut> win rate data.{" "}
-        <button
-          onClick={onReadMore}
-          className={clsx("underline", HOVER_CLASSES, TRANSITION_CLASSES)}
-          type="button"
-        >
-          Read more
+      <div className="flex items-center py-1.5 px-2 bg-neutral-100 dark:bg-neutral-800 rounded-lg lg:px-4">
+        <div className="grow">
+          <ImInfo className="inline relative bottom-0.5 mr-2" />
+          Grades below are based on{" "}
+          <LinkOut url="https://www.17lands.com/">17Lands</LinkOut> win rate
+          data.{" "}
+          <button
+            onClick={onReadMore}
+            className={clsx("underline", HOVER_CLASSES, TRANSITION_CLASSES)}
+            type="button"
+          >
+            Read more
+          </button>
+        </div>
+        <button onClick={() => setIsVisible(false)} type="button">
+          <IoClose
+            className={clsx("text-2xl", HOVER_CLASSES, TRANSITION_CLASSES)}
+            aria-label="Close Banner"
+          />
         </button>
       </div>
-      <button onClick={() => setIsVisible(false)} type="button">
-        <IoClose
-          className={clsx("text-2xl", HOVER_CLASSES, TRANSITION_CLASSES)}
-          aria-label="Close Banner"
-        />
-      </button>
     </div>
   );
 };
