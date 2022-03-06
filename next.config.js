5ca6ca0cb3f23bb36b65ff456531ffdd66bd9218
Youssef Moussaoui <yms@outlook.com>
2022-03-05 17:42:13 -0800

    Attempt rewrites for analytics
    Former-commit-id: fbc6d2a9a69009cabf38d210a3c95474751d125e

--- next.config.js
+++ next.config.js
@@ -2,18 +2,31 @@
 module.exports = {
   outputFileTracing: true,
   reactStrictMode: true,
-  redirects: async () => {
-    return [
-      {
-        source: "/",
-        destination: "/neo",
-        permanent: false,
-      },
-      {
-        source: "/:set/(all|wu|ub|br|rg|wg|wb|ur|bg|wr|ug)",
-        destination: "/:set",
-        permanent: false,
-      },
-    ];
+  redirects: async () => [
+    {
+      source: "/",
+      destination: "/neo",
+      permanent: false,
+    },
+    {
+      source: "/:set/(all|wu|ub|br|rg|wg|wb|ur|bg|wr|ug)",
+      destination: "/:set",
+      permanent: false,
+    },
+  ],
+  rewrites: async () => {
+    const rewrites = [];
+    const umamiServerName = process.env.UMAMI_SERVER_NAME;
+    if (umamiServerName) {
+      rewrites.push({
+        source: "/u.js",
+        destination: `https://${umamiServerName}/umami.js`,
+      });
+      rewrites.push({
+        source: "/api/collect",
+        destination: `https://${umamiServerName}/api/collect`,
+      });
+    }
+    return rewrites;
   },
 };
