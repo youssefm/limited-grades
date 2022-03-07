fdf42aa0cbf7ac9cf4217c1e758ca954548bb43b
Youssef Moussaoui <yms@outlook.com>
2022-03-06 21:20:06 -0800

    Improve border styling in card table
    Former-commit-id: 842f59ecf868122a6fe934c5b09cd28054661813

--- components/CardTable/CardTable.tsx
+++ components/CardTable/CardTable.tsx
@@ -15,8 +15,8 @@ import CardDetailModal from "./CardDetailModal";
 import CardView from "./CardView";
 
 const HEADER_BG_CLASSES = "bg-neutral-200 dark:bg-neutral-600";
-const HEADER_BORDER_CLASSES =
-  "border-b-2 border-neutral-800 dark:border-neutral-400";
+const HEADER_BORDER_CLASSES = "border-b-2 border-neutral-900";
+const BODY_BORDER_CLASSES = "border-b-2 border-white dark:border-neutral-900";
 
 interface Props {
   cardDictionary: CardTableDictionary;
@@ -29,7 +29,7 @@ const CardTable: FC<Props> = ({ cardDictionary, showSkeletons }) => {
   return (
     <>
       <div className="overflow-x-auto lg:overflow-x-visible">
-        <table className="w-full border-separate lg:table-fixed border-spacing-0">
+        <table className="w-full h-full border-separate lg:table-fixed border-spacing-0">
           <thead>
             <tr className="text-lg lg:sticky lg:top-0">
               <th
@@ -59,21 +59,27 @@ const CardTable: FC<Props> = ({ cardDictionary, showSkeletons }) => {
               <tr key={grade}>
                 <th
                   className={clsx(
-                    "pl-1 w-16 text-xl text-left lg:pl-4",
-                    "border-b border-l-4 border-b-neutral-300 dark:border-b-neutral-700",
+                    "p-0 w-16",
                     HEADER_BG_CLASSES,
-                    GRADE_BORDER_COLORS[grade],
+                    BODY_BORDER_CLASSES,
                     TRANSITION_CLASSES
                   )}
                 >
-                  {grade}
+                  <div
+                    className={clsx(
+                      "flex items-center pl-1 h-full text-xl border-l-4 lg:pl-4",
+                      GRADE_BORDER_COLORS[grade]
+                    )}
+                  >
+                    {grade}
+                  </div>
                 </th>
                 {ALL_COLUMNS.map((column) => (
                   <td
                     key={column}
                     className={clsx(
                       "py-2 px-1 align-top bg-neutral-100 dark:bg-neutral-800",
-                      "border-b border-neutral-200 dark:border-neutral-700",
+                      BODY_BORDER_CLASSES,
                       TRANSITION_CLASSES
                     )}
                   >
