42548133bce12df9c81bd1e3b1b1c799d751373b
Youssef Moussaoui <yms@outlook.com>
2022-03-11 17:57:53 -0800

    Move sitemap file up one level

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -18,4 +18,5 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 };
 
 // Default export to prevent next.js errors
-export default function Sitemap() {}
+const Sitemap = () => {};
+export default Sitemap;
