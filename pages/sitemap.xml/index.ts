91357bfc66b02bc20c28ee11cbf3eadbbe03a94e
Youssef Moussaoui <yms@outlook.com>
2022-03-11 17:55:23 -0800

    Try adding lastmod

--- pages/sitemap.xml/index.ts
+++ pages/sitemap.xml/index.ts
@@ -6,9 +6,12 @@ import { MagicSet } from "lib/types";
 const BASE_URL = "https://www.limitedgrades.com";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const fields: ISitemapField[] = [{ loc: BASE_URL, changefreq: "daily" }];
+  const now = new Date().toISOString();
+  const fields: ISitemapField[] = [
+    { loc: BASE_URL, lastmod: now, changefreq: "daily" },
+  ];
   for (const set of Object.values(MagicSet)) {
-    fields.push({ loc: `${BASE_URL}/${set}` });
+    fields.push({ loc: `${BASE_URL}/${set}`, lastmod: now });
   }
 
   return getServerSideSitemap(context, fields);
