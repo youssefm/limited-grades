29ae14b77b3da4d3e6ea2538fe7753ce30d7e4b3
Youssef Moussaoui <yms@outlook.com>
2022-03-16 12:49:01 -0700

    Simplify sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -6,12 +6,9 @@ import { MagicSet } from "lib/types";
 const BASE_URL = "https://www.limitedgrades.com";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const now = new Date().toISOString();
-  const fields: ISitemapField[] = [
-    { loc: BASE_URL, lastmod: now, changefreq: "daily" },
-  ];
+  const fields: ISitemapField[] = [{ loc: BASE_URL }];
   for (const set of Object.values(MagicSet)) {
-    fields.push({ loc: `${BASE_URL}/${set}`, lastmod: now });
+    fields.push({ loc: `${BASE_URL}/${set}` });
   }
 
   return getServerSideSitemap(context, fields);
