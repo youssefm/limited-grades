a12f89abba0cfc85eddfc388020e72331e2f2a60
Youssef Moussaoui <yms@outlook.com>
2022-03-23 01:22:10 -0700

    Avoid too many Collapsible re-renders
    Former-commit-id: 035145bfaa4ecee99118222c4ff1e2fe5c1084f8

--- src/components/common/Collapsible.tsx
+++ src/components/common/Collapsible.tsx
@@ -8,34 +8,35 @@ interface Props {
 }
 
 const Collapsible: FC<Props> = ({ isExpanded, id, className, children }) => {
-  const [isElementExpanded, setIsElementExpanded] = useState(isExpanded);
-  const [height, setHeight] = useState<number | undefined>(
-    isExpanded ? undefined : 0
-  );
+  const isMounted = useRef(false);
+  const pendingFirstExpansion = useRef(!isExpanded);
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const element = ref.current;
     if (element) {
-      if (isExpanded && !isElementExpanded) {
-        setHeight(element.scrollHeight);
-      }
-      if (!isExpanded && isElementExpanded) {
-        element.style.maxHeight = `${element.scrollHeight}px`;
-        // Force the browser to render the element by reading the scrollHeight
-        // We need maxHeight to be applied in the DOM for the transition to work
-        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-        element.scrollHeight;
-        setHeight(0);
+      if (isMounted.current) {
+        if (isExpanded) {
+          element.style.maxHeight = `${element.scrollHeight}px`;
+          pendingFirstExpansion.current = false;
+        } else {
+          element.style.maxHeight = `${element.scrollHeight}px`;
+          // Force the browser to render the element by reading the scrollHeight
+          // We need maxHeight to be applied in the DOM for the transition to work
+          // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+          element.scrollHeight;
+          element.style.maxHeight = "0";
+        }
+      } else {
+        isMounted.current = true;
       }
     }
-    setIsElementExpanded(isExpanded);
-  }, [isElementExpanded, isExpanded]);
+  }, [isExpanded]);
 
   const onTransitionEnd = (event: React.TransitionEvent<HTMLDivElement>) => {
     if (event.target === ref.current && isExpanded) {
       // Remove height constraint so element can regain responsiveness
-      setHeight(undefined);
+      ref.current.style.removeProperty("maxHeight");
     }
   };
 
@@ -46,7 +47,7 @@ const Collapsible: FC<Props> = ({ isExpanded, id, className, children }) => {
         "overflow-hidden transition-max-h ease-[ease]",
         className
       )}
-      style={{ maxHeight: height }}
+      style={pendingFirstExpansion.current ? { maxHeight: 0 } : undefined}
       onTransitionEnd={onTransitionEnd}
       ref={ref}
     >
