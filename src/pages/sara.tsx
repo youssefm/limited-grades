f012ea65a3ee379618781cd62db862f68b076690
Youssef Moussaoui <yms@outlook.com>
2022-03-27 16:21:23 -0700

    Allow cards to flip on land image page
    Former-commit-id: 3ac49a694f412fcd6dfca5b7d5b4f06aab71503c

--- src/pages/sara.tsx
+++ src/pages/sara.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
+import clsx from "clsx";
 import { upperFirst } from "lodash";
-import { GetStaticProps } from "next";
+import next, { GetStaticProps } from "next";
 import Head from "next/head";
 import { useCallback, useEffect, useState } from "react";
 
@@ -35,26 +36,50 @@ export const getStaticProps: GetStaticProps<StaticProps> = async () => {
 
 const Page = ({ imageUrls }: StaticProps) => {
   const [currentImageUrl, setCurrentImageUrl] = useState<string | undefined>();
+  const [activeImageUrls, setActiveImageUrls] = useState<string[]>([]);
 
-  const setNextImage = useCallback(
-    () =>
-      setCurrentImageUrl(
-        imageUrls[Math.floor(Math.random() * imageUrls.length)]
-      ),
-    [imageUrls, setCurrentImageUrl]
+  const getRandomImage = useCallback(
+    () => imageUrls[Math.floor(Math.random() * imageUrls.length)],
+    [imageUrls]
   );
 
-  useEffect(() => setNextImage(), [setNextImage]);
+  const moveToNextImage = useCallback(() => {
+    let nextImage = getRandomImage();
+    while (activeImageUrls.includes(nextImage)) {
+      nextImage = getRandomImage();
+    }
+    const nextActiveUrls = [...activeImageUrls, nextImage];
+    if (currentImageUrl) {
+      const currentIndex = activeImageUrls.indexOf(currentImageUrl);
+      if (currentIndex >= 0) {
+        setCurrentImageUrl(activeImageUrls[currentIndex + 1]);
+      }
+      if (currentIndex >= 1) {
+        nextActiveUrls.shift();
+      }
+    }
+    setActiveImageUrls(nextActiveUrls);
+  }, [getRandomImage, activeImageUrls, currentImageUrl]);
 
-  useEffect(
-    () =>
-      document.addEventListener("keydown", (e) => {
-        if (e.key === "ArrowRight") {
-          setNextImage();
-        }
-      }),
-    [setNextImage]
-  );
+  useEffect(() => {
+    const currentImage = getRandomImage();
+    let nextImage = getRandomImage();
+    while (nextImage === currentImage) {
+      nextImage = getRandomImage();
+    }
+    setActiveImageUrls([currentImage, getRandomImage()]);
+    setCurrentImageUrl(currentImage);
+  }, [getRandomImage]);
+
+  useEffect(() => {
+    const handleRightArrowPress = (e: KeyboardEvent) => {
+      if (e.key === "ArrowRight") {
+        moveToNextImage();
+      }
+    };
+    document.addEventListener("keydown", handleRightArrowPress);
+    return () => document.removeEventListener("keydown", handleRightArrowPress);
+  }, [moveToNextImage]);
 
   return (
     <>
@@ -66,11 +91,24 @@ const Page = ({ imageUrls }: StaticProps) => {
           <h1 className="mb-4 text-4xl text-center text-neutral-100">
             For Sara 🙂
           </h1>
-          {currentImageUrl && (
-            <button type="button" onClick={setNextImage}>
-              <img src={currentImageUrl} width="720" height="1020" />
+          <div className="relative w-[720px] h-[1020px]">
+            <button onClick={moveToNextImage} type="button">
+              {activeImageUrls.map((imageUrl) => (
+                <div
+                  key={imageUrl}
+                  className={clsx(
+                    "absolute inset-0 transition-all duration-500 ease-in-out backface-invisible",
+                    {
+                      "rotate-y-0": imageUrl === currentImageUrl,
+                      "rotate-y-180": imageUrl !== currentImageUrl,
+                    }
+                  )}
+                >
+                  <img src={imageUrl} width="720" height="1020" />
+                </div>
+              ))}
             </button>
-          )}
+          </div>
         </div>
       </div>
     </>
