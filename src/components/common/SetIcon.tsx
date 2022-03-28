c7bd4af96edaa09f979671738141c8292e92d54c
Youssef Moussaoui <yms@outlook.com>
2022-03-28 13:53:26 -0700

    Add a component for SetIcons
    Former-commit-id: 8144c1562e21556f1328dacb61e4b06a11b8a6e7

---
+++ src/components/common/SetIcon.tsx
@@ -0,0 +1,48 @@
+import AfrIcon from "keyrune/svg/afr.svg";
+import AkrIcon from "keyrune/svg/akr.svg";
+import DomIcon from "keyrune/svg/dom.svg";
+import GrnIcon from "keyrune/svg/grn.svg";
+import IkoIcon from "keyrune/svg/iko.svg";
+import KhmIcon from "keyrune/svg/khm.svg";
+import KlrIcon from "keyrune/svg/klr.svg";
+import MidIcon from "keyrune/svg/mid.svg";
+import NeoIcon from "keyrune/svg/neo.svg";
+import DefaultIcon from "keyrune/svg/pmtg1.svg";
+import RnaIcon from "keyrune/svg/rna.svg";
+import StxIcon from "keyrune/svg/stx.svg";
+import VowIcon from "keyrune/svg/vow.svg";
+import WarIcon from "keyrune/svg/war.svg";
+import ZnrIcon from "keyrune/svg/znr.svg";
+import { FC, SVGProps } from "react";
+
+import { MagicSet } from "lib/types";
+
+const SET_ICONS: Record<MagicSet, FC<SVGProps<SVGSVGElement>>> = {
+  [MagicSet.NEON_DYNASTY]: NeoIcon,
+  [MagicSet.CRIMSON_VOW]: VowIcon,
+  [MagicSet.MIDNIGHT_HUNT]: MidIcon,
+  [MagicSet.FORGOTTEN_REALM]: AfrIcon,
+  [MagicSet.STRIXHAVEN]: StxIcon,
+  [MagicSet.KALDHEIM]: KhmIcon,
+  [MagicSet.ZENDIKAR]: ZnrIcon,
+  [MagicSet.IKORIA]: IkoIcon,
+  [MagicSet.WAR_OF_THE_SPARK]: WarIcon,
+  [MagicSet.RAVNICA_ALLEGIANCE]: RnaIcon,
+  [MagicSet.GUILDS_OF_RAVNICA]: GrnIcon,
+  [MagicSet.DOMINARIA]: DomIcon,
+  [MagicSet.AMONKHET]: AkrIcon,
+  [MagicSet.KALADESH]: KlrIcon,
+  [MagicSet.ARENA_CUBE]: DefaultIcon,
+  [MagicSet.DOUBLE_FEATURE]: DefaultIcon,
+};
+
+interface Props {
+  set: MagicSet;
+}
+
+const SetIcon: FC<Props> = ({ set }) => {
+  const SvgIcon = SET_ICONS[set];
+  return <SvgIcon className="inline-block w-[1.28571429em] text-center" />;
+};
+
+export default SetIcon;
