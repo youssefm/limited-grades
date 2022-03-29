dd4f41917d37069fea4036d1f909cf8cc0b962b3
Youssef Moussaoui <yms@outlook.com>
2022-03-29 08:32:45 -0700

    Replace card type icons with inline SVGs
    Former-commit-id: dee6248a7fdd0092d2b7f8407f18d72e068dc3b1

--- src/components/FilterBar/CardTypeFilter.tsx
+++ src/components/FilterBar/CardTypeFilter.tsx
@@ -1,29 +1,38 @@
+import CreatureIcon from "mana-font/svg/creature.svg";
+import EnchantmentIcon from "mana-font/svg/enchantment.svg";
+import InstantIcon from "mana-font/svg/instant.svg";
+import LandIcon from "mana-font/svg/land.svg";
 import { FC } from "react";
 
 import { CardType } from "lib/types";
 
 import IconFilterGroup from "./IconFilterGroup";
 
+const ICON_PROPS = {
+  width: "1.28571429em",
+  height: "1em",
+};
+
 const FILTERS = [
   {
     label: "creatures",
     values: [CardType.CREATURE],
-    icon: <i className="ms ms-creature" />,
+    icon: <CreatureIcon {...ICON_PROPS} />,
   },
   {
     label: "instants and sorceries",
     values: [CardType.INSTANT, CardType.SORCERY],
-    icon: <i className="ms ms-instant" />,
+    icon: <InstantIcon {...ICON_PROPS} />,
   },
   {
     label: "artifacts, enchantments, and planeswalkers",
     values: [CardType.ARTIFACT, CardType.ENCHANTMENT, CardType.PLANESWALKER],
-    icon: <i className="ms ms-enchantment" />,
+    icon: <EnchantmentIcon {...ICON_PROPS} />,
   },
   {
     label: "lands",
     values: [CardType.LAND],
-    icon: <i className="ms ms-land" />,
+    icon: <LandIcon {...ICON_PROPS} />,
   },
 ];
 
