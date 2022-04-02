5550e0cd9788c633e66560b738274f59b44a9b5d
Youssef Moussaoui <yms@outlook.com>
2022-04-01 23:12:58 -0700

    Separate out Select from IconSelect
    Former-commit-id: 3c231a7a9147bbd22fa75d48fed28f9ffa55a8b3

--- src/components/FilterBar/IconSelect.tsx
+++ src/components/FilterBar/IconSelect.tsx
@@ -1,32 +1,7 @@
-import { omit } from "lodash";
 import { FC, ReactElement, ReactNode, useCallback } from "react";
-import Select, {
-  Colors,
-  components,
-  OptionProps,
-  SingleValueProps,
-} from "react-select";
-import { StateManagerProps } from "react-select/dist/declarations/src/stateManager";
+import { components, OptionProps, SingleValueProps } from "react-select";
 
-import useDarkMode from "hooks/useDarkMode";
-
-const getDarkModeColors = (baseColors: Colors) => ({
-  neutral0: "#525252", // tailwind neutral-600
-  neutral5: baseColors.neutral90,
-  neutral10: baseColors.neutral90,
-  neutral20: baseColors.neutral80,
-  neutral30: baseColors.neutral70,
-  neutral40: baseColors.neutral60,
-  neutral50: baseColors.neutral50,
-  neutral60: baseColors.neutral40,
-  neutral70: baseColors.neutral30,
-  neutral80: baseColors.neutral20,
-  neutral90: baseColors.neutral10,
-  primary: "#e78b09", // tailwind amber-550
-  primary75: "#e28408",
-  primary50: "#de7e07",
-  primary25: "#d97706", // tailwind amber-600
-});
+import Select from "components/common/Select";
 
 interface Props<T> {
   value: T;
@@ -47,8 +22,6 @@ const IconSelect = <T extends unknown>({
   instanceId,
   className,
 }: Props<T>) => {
-  const [darkModeEnabled] = useDarkMode();
-
   interface TOption {
     value: T;
     label: string;
@@ -91,64 +64,17 @@ const IconSelect = <T extends unknown>({
     [OptionView]
   );
 
-  const selectProps: StateManagerProps<TOption, false> = {
-    value: { value, label: getLabel(value) },
-    onChange: (selectedOption: TOption | null) => {
-      if (selectedOption) {
-        onChange(selectedOption.value);
-      }
-    },
-    options: options.map((option: T) => ({
-      value: option,
-      label: getLabel(option),
-    })),
-    isMulti: false,
-    components: { SingleValue, Option },
-    instanceId,
-    className,
-    classNamePrefix: "rs",
-    styles: {
-      control: (provided) => ({
-        ...provided,
-        transitionDuration: "150ms",
-        transitionProperty: "background-color border-color",
-      }),
-      dropdownIndicator: (provided) => ({
-        ...provided,
-        transition: "color 150ms",
-      }),
-      indicatorSeparator: (provided) => ({
-        ...provided,
-        transition: "background-color 150ms",
-      }),
-      input: (provided) => omit(provided, "color"),
-      option: (provided, state) => {
-        const optionStyles = {
-          ...provided,
-          cursor: "pointer",
-        };
-
-        if (darkModeEnabled && state.isSelected) {
-          delete optionStyles.color;
-        }
-
-        return optionStyles;
-      },
-      singleValue: (provided) => omit(provided, "color"),
-    },
-  };
-
-  if (darkModeEnabled) {
-    selectProps.theme = (theme) => ({
-      ...theme,
-      colors: {
-        ...theme.colors,
-        ...getDarkModeColors(theme.colors),
-      },
-    });
-  }
-
-  return <Select {...selectProps} />;
+  return (
+    <Select
+      value={value}
+      onChange={onChange}
+      options={options}
+      getLabel={getLabel}
+      components={{ SingleValue, Option }}
+      instanceId={instanceId}
+      className={className}
+    />
+  );
 };
 
 export default IconSelect;
