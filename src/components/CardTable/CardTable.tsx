142a03692ab0af4e1311f109bc780e3942bb7695
Youssef Moussaoui <yms@outlook.com>
2022-04-01 18:19:19 -0700

    Move embargo banner into CardDetail implementation

--- src/components/CardTable/CardTable.tsx
+++ src/components/CardTable/CardTable.tsx
@@ -1,9 +1,10 @@
 import { FC, useState } from "react";
 
+import CardDetail from "components/common/CardDetail";
+import Modal from "components/common/Modal";
 import { CardTableDictionary } from "lib/table";
 import { Card, MagicSet } from "lib/types";
 
-import CardDetailModal from "./CardDetailModal";
 import CardTableCore from "./CardTableCore";
 
 interface Props {
@@ -22,11 +23,15 @@ const CardTable: FC<Props> = ({ cardDictionary, set, showSkeletons }) => {
         showSkeletons={showSkeletons}
         onClickCard={setModalCard}
       />
-      <CardDetailModal
-        card={modalCard}
-        set={set}
-        onClose={() => setModalCard(undefined)}
-      />
+      {modalCard && (
+        <Modal
+          title={modalCard.name}
+          onClose={() => setModalCard(undefined)}
+          className="w-full lg:w-auto"
+        >
+          <CardDetail card={modalCard} set={set} />
+        </Modal>
+      )}
     </>
   );
 };
