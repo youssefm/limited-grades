4394c691517e0ceae8a426303b3bf76411cbfb94
Youssef Moussaoui <yms@outlook.com>
2022-04-01 18:19:19 -0700

    Move embargo banner into CardDetail implementation
    Former-commit-id: 142a03692ab0af4e1311f109bc780e3942bb7695

--- src/components/common/CardDetail/CardDetail.tsx
+++ src/components/common/CardDetail/CardDetail.tsx
@@ -1,32 +1,46 @@
 import { FC } from "react";
+import { ImInfo } from "react-icons/im";
 
-import { Card } from "lib/types";
+import { isSetUnderEmbargo } from "lib/sets";
+import { Card, MagicSet } from "lib/types";
 
+import Banner from "../Banner";
 import CardImage from "./CardImage";
 import DeckAnalysisTable from "./DeckAnalysisTable";
 import DetailedStatsTable from "./DetailedStatsTable";
 
 interface Props {
   card: Card;
-  showStats: boolean;
+  set: MagicSet;
 }
 
-const CardDetail: FC<Props> = ({ card, showStats }) => (
-  <div className="flex flex-col gap-6 lg:flex-row">
-    <div className="self-center mt-9 lg:self-start">
-      <CardImage card={card} />
-    </div>
-    <div className="self-stretch">
-      <div className="mb-2 text-lg">Deck Analysis</div>
-      <DeckAnalysisTable card={card} showStats={showStats} />
-    </div>
-    {showStats && (
-      <div className="self-stretch">
-        <div className="mb-2 text-lg">Full 17Lands Stats</div>
-        <DetailedStatsTable card={card} />
+const CardDetail: FC<Props> = ({ card, set }) => {
+  const underEmbargo = isSetUnderEmbargo(set);
+  return (
+    <>
+      {underEmbargo && (
+        <Banner dismissable={false}>
+          <ImInfo className="inline relative bottom-0.5 mr-2" />
+          Detailed 17Lands stats are not yet available for this set
+        </Banner>
+      )}
+      <div className="flex flex-col gap-6 lg:flex-row">
+        <div className="self-center mt-9 lg:self-start">
+          <CardImage card={card} />
+        </div>
+        <div className="self-stretch">
+          <div className="mb-2 text-lg">Deck Analysis</div>
+          <DeckAnalysisTable card={card} showStats={!underEmbargo} />
+        </div>
+        {!underEmbargo && (
+          <div className="self-stretch">
+            <div className="mb-2 text-lg">Full 17Lands Stats</div>
+            <DetailedStatsTable card={card} />
+          </div>
+        )}
       </div>
-    )}
-  </div>
-);
+    </>
+  );
+};
 
 export default CardDetail;
