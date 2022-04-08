36c9ddcaf2ddc9e7ca33917959b83d214723877d
Youssef Moussaoui <yms@outlook.com>
2022-04-08 12:15:35 -0700

    Simplify implementation of IconSelect
    Former-commit-id: 4a968cc4100aa4ce3f91c909236e8dc7b3ca72b4

--- src/components/FilterBar/IconSelect.tsx
+++ src/components/FilterBar/IconSelect.tsx
@@ -1,5 +1,4 @@
-import { FC, ReactElement, ReactNode, useCallback } from "react";
-import { components, OptionProps, SingleValueProps } from "react-select";
+import { ReactNode, useCallback } from "react";
 
 import Select, { Props as SelectProps } from "components/common/Select";
 
@@ -11,49 +10,24 @@ const IconSelect = <T extends unknown>({
   getIcon,
   ...extraProps
 }: Props<T>) => {
-  interface TOption {
-    value: T;
-    label: string;
-  }
-
-  const OptionView: FC<{ optionValue: T }> = useCallback(
-    ({ optionValue, children }) => {
-      const icon = getIcon(optionValue);
+  const formatOptionLabel = useCallback(
+    ({ value, label }) => {
+      const icon = getIcon(value);
       if (!icon) {
-        // This type assertion is not technically correct but this is a workaround
-        // for a Typescript issue: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/18051#issuecomment-485151038
-        return children as ReactElement;
+        return label;
       }
 
       return (
         <div className="flex">
           {icon}
-          <span className="ml-2">{children}</span>
+          <span className="ml-2">{label}</span>
         </div>
       );
     },
     [getIcon]
   );
 
-  const SingleValue: FC<SingleValueProps<TOption, false>> = useCallback(
-    ({ children, ...props }) => (
-      <components.SingleValue {...props}>
-        <OptionView optionValue={props.data.value}>{children}</OptionView>
-      </components.SingleValue>
-    ),
-    [OptionView]
-  );
-
-  const Option: FC<OptionProps<TOption, false>> = useCallback(
-    ({ children, ...props }) => (
-      <components.Option {...props}>
-        <OptionView optionValue={props.data.value}>{children}</OptionView>
-      </components.Option>
-    ),
-    [OptionView]
-  );
-
-  return <Select components={{ SingleValue, Option }} {...extraProps} />;
+  return <Select formatOptionLabel={formatOptionLabel} {...extraProps} />;
 };
 
 export default IconSelect;
