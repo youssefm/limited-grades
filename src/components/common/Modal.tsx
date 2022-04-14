642985cd8e3fb452a9930f4a120ae6dc44de3b52
Youssef Moussaoui <yms@outlook.com>
2022-04-14 11:35:15 -0700

    Animate modals in when they first appear
    Former-commit-id: 319286642776907c99ffefc8f0b3348896b97bd5

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,6 +1,6 @@
-import { Dialog } from "@headlessui/react";
+import { Dialog, Transition } from "@headlessui/react";
 import clsx from "clsx";
-import { FC } from "react";
+import { FC, Fragment } from "react";
 import { IoClose } from "react-icons/io5";
 
 import { HOVER_CLASSES, TRANSITION_CLASSES } from "lib/styles";
@@ -19,29 +19,44 @@ const Modal: FC<Props> = ({
   className,
   children,
 }) => (
-  <Dialog
-    open
-    onClose={onClose}
-    className="grid fixed inset-0 place-items-center"
-    initialFocus={initialFocus}
-  >
-    <Dialog.Overlay className="fixed inset-0 z-10 bg-black/60 backdrop-blur-xs" />
-    <div
-      className={clsx(
-        "overflow-y-auto z-20 max-h-full dark:text-neutral-100",
-        "bg-white dark:bg-neutral-900 rounded-lg border border-black shadow-xl",
-        className
-      )}
+  <Transition appear show as={Fragment}>
+    <Dialog
+      onClose={onClose}
+      className="grid fixed inset-0 place-items-center"
+      initialFocus={initialFocus}
     >
-      <Dialog.Title className="flex items-center p-4 text-2xl border-b border-neutral-200 dark:border-black">
-        <span className="grow font-medium">{title}</span>
-        <button onClick={onClose} type="button" aria-label="Close Modal">
-          <IoClose className={clsx(HOVER_CLASSES, TRANSITION_CLASSES)} />
-        </button>
-      </Dialog.Title>
-      <div className="p-4">{children}</div>
-    </div>
-  </Dialog>
+      <Transition.Child
+        enter="transition duration-300 ease-[ease]"
+        enterFrom="opacity-0 backdrop-opacity-0"
+        enterTo="opacity-100 backdrop-opacity-100"
+        as={Fragment}
+      >
+        <Dialog.Overlay className="fixed inset-0 z-10 bg-black/60 backdrop-blur-xs" />
+      </Transition.Child>
+      <Transition.Child
+        enter="transition-opacity duration-300 ease-[ease]"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        as={Fragment}
+      >
+        <div
+          className={clsx(
+            "overflow-y-auto z-20 max-h-full dark:text-neutral-100",
+            "bg-white dark:bg-neutral-900 rounded-lg border border-black shadow-xl",
+            className
+          )}
+        >
+          <Dialog.Title className="flex items-center p-4 text-2xl border-b border-neutral-200 dark:border-black">
+            <span className="grow font-medium">{title}</span>
+            <button onClick={onClose} type="button" aria-label="Close Modal">
+              <IoClose className={clsx(HOVER_CLASSES, TRANSITION_CLASSES)} />
+            </button>
+          </Dialog.Title>
+          <div className="p-4">{children}</div>
+        </div>
+      </Transition.Child>
+    </Dialog>
+  </Transition>
 );
 
 export default Modal;
