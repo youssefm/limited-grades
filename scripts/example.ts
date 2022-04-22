8b9b8cdcc8b3712f60e4d706b945c3d175e37f0c
Youssef Moussaoui <yms@outlook.com>
2022-04-22 11:09:16 -0700

    Rename test script to avoid collision
    Former-commit-id: dc37b45ee9eb767b139e2222f5276ae9485523ee

--- scripts/example.ts
+++ scripts/example.ts
@@ -1,7 +1,6 @@
 import { SCRYFALL_FILE_INDEX } from "lib/scryfall";
 
 const main = async () => {
-  // const index = await fetchScryfallIndex(MagicSet.MIDNIGHT_HUNT);
   const index = await SCRYFALL_FILE_INDEX.get();
   console.log(index.lookupCard("Organ Hoarder"));
 };
