69d742fcf284793af60579a5784d3b3f4c8a3ead
Youssef Moussaoui <yms@outlook.com>
2022-04-22 10:35:33 -0700

    Add basic tests for search.ts
    Former-commit-id: 9f883b1a60eaf1a6ce631e4629458eabc5d83ae7

---
+++ test/lib/search.test.ts
@@ -0,0 +1,31 @@
+import SearchIndex from "lib/search";
+
+describe("Search", () => {
+  it("full match", () => {
+    const index = new SearchIndex(["fred", "john", "sara"], (item) => item);
+    const match = index.search("sara");
+    expect(match).toEqual([
+      {
+        item: "sara",
+        match: { atStartOfWord: true, startPosition: 0, endPosition: 4 },
+      },
+    ]);
+  });
+
+  it("partial match", () => {
+    const index = new SearchIndex(["fred", "john", "sara"], (item) => item);
+    const match = index.search("ara");
+    expect(match).toEqual([
+      {
+        item: "sara",
+        match: { atStartOfWord: false, startPosition: 1, endPosition: 4 },
+      },
+    ]);
+  });
+
+  it("no match", () => {
+    const index = new SearchIndex(["fred", "john", "sara"], (item) => item);
+    const match = index.search("sora");
+    expect(match).toHaveLength(0);
+  });
+});
