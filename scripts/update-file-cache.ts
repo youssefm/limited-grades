6ed89221b87f83624226dcd2c750fc226a1d14ab
Youssef Moussaoui <yms@outlook.com>
2022-04-22 22:34:49 -0700

    Remove unnecessary stuff added for trying to enable scripts

--- scripts/update-file-cache.ts
+++ scripts/update-file-cache.ts
@@ -1,10 +1,7 @@
-import { loadEnvConfig } from "@next/env";
-
 import { FILE_CACHE, REDIS_CACHE } from "lib/cache";
 import MagicSet from "lib/MagicSet";
 
 const main = async () => {
-  loadEnvConfig(process.cwd());
   for (const set of MagicSet.ALL) {
     const cardStore = await REDIS_CACHE.get(set.code);
     if (cardStore) {
@@ -12,7 +9,6 @@ const main = async () => {
       await FILE_CACHE.set(set.code, cardStore);
     }
   }
-  await REDIS_CACHE.close();
   console.log("Done updating all file caches!");
 };
 
