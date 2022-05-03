f38f27c6e50dc417efc87d90fe5a1c9861ca91d9
Youssef Moussaoui <yms@outlook.com>
2022-05-03 14:06:30 -0700

    Delete unnecessary code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,9 @@
-import Document, {
-  DocumentContext,
-  Head,
-  Html,
-  Main,
-  NextScript,
-} from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 import DarkModeInitializer from "components/common/DarkModeInitializer";
 import HideBannerInitializer from "components/common/HideBannerInitializer";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
   render() {
     return (
       <Html lang="en">
