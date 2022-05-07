2672e30f2ad7bc031efc0c4b611258e62a02b577
Youssef Moussaoui <yms@outlook.com>
2022-05-07 09:27:14 -0700

    Add new admin actions for checking and populating redis cache
    Former-commit-id: c17ed3a90197974f49b43474a69c3d18a4d816bc

--- src/lib/admin.ts
+++ src/lib/admin.ts
@@ -1,40 +1,56 @@
 import { FILE_CACHE, REDIS_CACHE, REDIS_CLIENT } from "./cache";
+import getCardStore from "./getCardStore";
 import MagicSet from "./MagicSet";
 import { fetchScryfallIndex, SCRYFALL_FILE_INDEX } from "./scryfall";
 
-export const ACTIONS: Record<string, (output: any[]) => Promise<void>> =
-  process.env.ADMIN_ENABLED === "true"
-    ? {
-        "clear-redis-cache": async (output) => {
-          await REDIS_CACHE.clear();
-          output.push("Redis cache cleared!");
-        },
-        "delete-snc-cache": async (output) => {
-          const client = await REDIS_CLIENT.get();
-          await client.del("snc");
-          output.push("SNC cache value deleted!");
-        },
-        "fetch-scryfall-card": async (output) => {
-          const index = await fetchScryfallIndex(MagicSet.MIDNIGHT_HUNT);
-          const card = index.lookupCard("Organ Hoarder");
-          output.push(card);
-        },
-        "lookup-scryfall-card": async (output) => {
-          const index = await SCRYFALL_FILE_INDEX.get();
-          const card = index.lookupCard("Organ Hoarder");
-          output.push(card);
-        },
-        "update-file-cache": async (output) => {
-          for (const set of MagicSet.ALL) {
-            const cardStore = await REDIS_CACHE.get(set.code);
-            if (cardStore) {
-              output.push(`Updating file cache for ${set.code.toUpperCase()}`);
-              await FILE_CACHE.set(set.code, cardStore);
-            }
-          }
-          output.push("Done updating all file caches!");
-        },
+const ACTIONS: Record<string, (output: any[]) => Promise<void>> = {
+  "check-redis-keys": async (output) => {
+    const client = await REDIS_CLIENT.get();
+    for (const set of MagicSet.ALL) {
+      const exists = await client.exists(set.code);
+      output.push(
+        `${set.code.toUpperCase()}: ${exists ? "Cached" : "Not Cached"}`
+      );
+    }
+  },
+  "clear-redis-cache": async (output) => {
+    await REDIS_CACHE.clear();
+    output.push("Redis cache cleared!");
+  },
+  "delete-snc-cache": async (output) => {
+    const client = await REDIS_CLIENT.get();
+    await client.del("snc");
+    output.push("SNC cache value deleted!");
+  },
+  "fetch-scryfall-card": async (output) => {
+    const index = await fetchScryfallIndex(MagicSet.MIDNIGHT_HUNT);
+    const card = index.lookupCard("Organ Hoarder");
+    output.push(card);
+  },
+  "lookup-scryfall-card": async (output) => {
+    const index = await SCRYFALL_FILE_INDEX.get();
+    const card = index.lookupCard("Organ Hoarder");
+    output.push(card);
+  },
+  "populate-redis-cache": async (output) => {
+    for (const set of MagicSet.ALL) {
+      await getCardStore(set, REDIS_CACHE);
+      output.push(`${set.code.toUpperCase()}: Cache Populated`);
+    }
+  },
+  "update-file-cache": async (output) => {
+    for (const set of MagicSet.ALL) {
+      const cardStore = await REDIS_CACHE.get(set.code);
+      if (cardStore) {
+        output.push(`Updating file cache for ${set.code.toUpperCase()}`);
+        await FILE_CACHE.set(set.code, cardStore);
       }
-    : {};
+    }
+    output.push("Done updating all file caches!");
+  },
+};
 
-export const ACTION_KEYS = Object.keys(ACTIONS);
+export const ADMIN_ACTIONS =
+  process.env.ADMIN_ENABLED === "true" ? ACTIONS : {};
+
+export const ADMIN_ACTION_KEYS = Object.keys(ACTIONS);
--- src/pages/api/admin/index.ts
+++ src/pages/api/admin/index.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-import { ACTION_KEYS } from "lib/admin";
+import { ADMIN_ACTION_KEYS } from "lib/admin";
 
 const handler = async (request: NextApiRequest, response: NextApiResponse) => {
-  response.status(200).json(ACTION_KEYS);
+  response.status(200).json(ADMIN_ACTION_KEYS);
 };
 
 export default handler;
