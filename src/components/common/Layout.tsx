40d79d251ab553ff3ca0a6bbb96f998bc7e1be9f
Youssef Moussaoui <yms@outlook.com>
2022-05-11 12:59:17 -0700

    Switch color scheme and theme color in concert with dark mode
    Former-commit-id: f3b013bb63a9575aaa816ce376a6a991581aa2d4

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,58 +1,68 @@
 import Head from "next/head";
 import { FC } from "react";
 
-import { DarkModeProvider } from "hooks/useDarkMode";
+import useDarkMode from "hooks/useDarkMode";
 
-const Layout: FC = ({ children }) => (
-  <>
-    <Head>
-      <title>Limited Grades</title>
-      <meta
-        name="description"
-        content="Visualize MTG draft win rates as a tier list, powered by 17Lands data"
-      />
-      <meta
-        property="og:image"
-        content="https://www.limitedgrades.com/android-chrome-512x512.png"
-      />
-      <meta property="og:image:type" content="image/png" />
-      <meta property="og:image:width" content="512" />
-      <meta property="og:image:height" content="512" />
-      <meta name="color-scheme" content="light dark" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <link rel="manifest" href="manifest.json" />
-      {/* NOTE: This depends on the current implementation of NextJS and may need
+const Layout: FC = ({ children }) => {
+  const [darkModeEnabled] = useDarkMode();
+  return (
+    <>
+      <Head>
+        <title>Limited Grades</title>
+        <meta
+          name="description"
+          content="Visualize MTG draft win rates as a tier list, powered by 17Lands data"
+        />
+        <meta
+          property="og:image"
+          content="https://www.limitedgrades.com/android-chrome-512x512.png"
+        />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="512" />
+        <meta property="og:image:height" content="512" />
+        <meta
+          name="color-scheme"
+          content={darkModeEnabled ? "dark" : "light"}
+        />
+        <meta
+          name="theme-color"
+          content={darkModeEnabled ? "#171717" : "#ffffff"}
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="manifest.json" />
+        {/* NOTE: This depends on the current implementation of NextJS and may need
           to be revisited if NextJS changes the location of the font files it references
       */}
-      <link
-        rel="preload"
-        href="/_next/static/media/Beleren2016SmallCaps-Bold.012bd2dd.woff"
-        as="font"
-        type="font/woff2"
-        crossOrigin="anonymous"
-      />
-      {/* Prefetch DNS information for Scryfall so first card image fetched loads faster */}
-      <link rel="dns-prefetch" href="https://c1.scryfall.com" />
-    </Head>
+        <link
+          rel="preload"
+          href="/_next/static/media/Beleren2016SmallCaps-Bold.012bd2dd.woff"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        {/* Prefetch DNS information for Scryfall so first card image fetched loads faster */}
+        <link rel="dns-prefetch" href="https://c1.scryfall.com" />
+      </Head>
 
-    <DarkModeProvider>{children}</DarkModeProvider>
-  </>
-);
+      {children}
+    </>
+  );
+};
 
 export default Layout;
