02db4d962031676158b05516378cc5ba41700ada
Youssef Moussaoui <yms@outlook.com>
2022-05-15 17:45:52 -0700

    Factor out helper functions for writing JSON files

--- src/lib/util.server.ts
+++ src/lib/util.server.ts
@@ -1,9 +1,9 @@
-import { readFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 
-import { ungzip } from "node-gzip";
+import { gzip, ungzip } from "node-gzip";
 
 // Adapted from https://github.com/lodash/lodash/blob/2da024c3b4f9947a48517639de7560457cd4ec6c/.internal/createRound.js
-export const round = (n: number, precision: number = 0) => {
+export const round = (n: number, precision: number = 0): number => {
   let [significand, exponent] = `${n}e`.split("e");
   const value = Math.round(
     Number(`${significand}e${Number(exponent) + precision}`)
@@ -14,14 +14,24 @@ export const round = (n: number, precision: number = 0) => {
 };
 
 export const readJsonFile = async <T>(filePath: string): Promise<T> => {
-  let json;
+  let buffer = await readFile(filePath);
   if (filePath.endsWith(".gz")) {
-    const buffer = await ungzip(await readFile(filePath));
-    json = buffer.toString("utf8");
-  } else {
-    json = await readFile(filePath, "utf8");
+    buffer = await ungzip(buffer);
   }
+  const json = buffer.toString("utf8");
   return JSON.parse(json);
+};
+
+export const writeJsonFile = async (
+  filePath: string,
+  value: any
+): Promise<void> => {
+  const json = JSON.stringify(value);
+  let buffer = Buffer.from(json, "utf8");
+  if (filePath.endsWith(".gz")) {
+    buffer = await gzip(buffer);
+  }
+  await writeFile(filePath, buffer);
 };
 
 export class LazySingleton<T> {
@@ -33,7 +43,7 @@ export class LazySingleton<T> {
     this.#creator = creator;
   }
 
-  get() {
+  get(): T {
     if (this.#instance === null) {
       this.#instance = this.#creator();
     }
