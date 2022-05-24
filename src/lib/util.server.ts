64bccff4e1a2eb53a0ea2cc834786bec5884c29c
Youssef Moussaoui <yms@outlook.com>
2022-05-24 09:22:24 -0700

    Incorporate new-grades general updates
    Former-commit-id: 1062c7848c0c8a5443d9eb236c92343fe058e025

--- src/lib/util.server.ts
+++ src/lib/util.server.ts
@@ -72,3 +72,34 @@ export const lookupEnumByValue = <T extends string>(
   Enum: Record<string, T>,
   value: string
 ): T | undefined => Object.values(Enum).find((v) => v === value);
+
+export function* zip<T1, T2>(
+  iterable1: Iterable<T1>,
+  iterable2: Iterable<T2>
+): Generator<[T1, T2]> {
+  const iterator1 = iterable1[Symbol.iterator]();
+  const iterator2 = iterable2[Symbol.iterator]();
+
+  let iterator1Result;
+  let iterator2Result;
+  for (;;) {
+    iterator1Result = iterator1.next();
+    iterator2Result = iterator2.next();
+    if (iterator1Result.done || iterator2Result.done) {
+      break;
+    }
+    yield [iterator1Result.value, iterator2Result.value];
+  }
+}
+
+export const indexBy = <T>(
+  iterable: Iterable<T>,
+  getKey: (item: T) => string
+) => {
+  const result: Record<string, T> = {};
+  for (const item of iterable) {
+    const key = getKey(item);
+    result[key] = item;
+  }
+  return result;
+};
