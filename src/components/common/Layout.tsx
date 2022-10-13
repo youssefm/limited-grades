93e9040789e21601bec115999b96e20f5b48a6a1
Youssef Moussaoui <yms@outlook.com>
2022-10-12 21:15:21 -0700

    Add generated og image

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -15,11 +15,11 @@ const Layout: FC = ({ children }) => {
         />
         <meta
           property="og:image"
-          content="https://www.limitedgrades.com/android-chrome-512x512.png"
+          content="https://www.limitedgrades.com/og.png"
         />
         <meta property="og:image:type" content="image/png" />
-        <meta property="og:image:width" content="512" />
-        <meta property="og:image:height" content="512" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <meta
           name="color-scheme"
           content={darkModeEnabled ? "dark" : "light"}
