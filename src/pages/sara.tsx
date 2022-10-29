d6305423a68ea1b7b49d4978702b8e1e4c3e5e35
Youssef Moussaoui <yms@outlook.com>
2022-10-29 13:40:32 -0700

    Try disabling Swiper elements to test speed

--- src/pages/sara.tsx
+++ src/pages/sara.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable jsx-a11y/alt-text */
 import { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import SwiperCore from "swiper";
-import "swiper/css";
-import { Swiper, SwiperSlide } from "swiper/react";
+// import "swiper/css";
+// import { Swiper, SwiperSlide } from "swiper/react";
 
 import { LAND_IMAGES } from "lib/scryfall";
 
@@ -20,7 +20,7 @@ export const getStaticProps: GetStaticProps<StaticProps> = async () => ({
 
 const Page: NextPage<StaticProps> = ({ imageUrls }) => {
   const swiperRef = useRef<SwiperCore>();
-  const [activeImageUrls, setActiveImageUrls] = useState<string[]>([]);
+  // const [activeImageUrls, setActiveImageUrls] = useState<string[]>([]);
 
   const getRandomImageUrl = useCallback(
     () => imageUrls[Math.floor(Math.random() * imageUrls.length)]!,
@@ -37,29 +37,29 @@ const Page: NextPage<StaticProps> = ({ imageUrls }) => {
       initialActiveImageUrls.push(imageUrl);
     }
 
-    setActiveImageUrls(initialActiveImageUrls);
+    // setActiveImageUrls(initialActiveImageUrls);
   }, [getRandomImageUrl]);
 
-  const onSlideChange = () => {
-    const swiper = swiperRef.current;
-    if (swiper) {
-      if (swiper.activeIndex <= 2) {
-        let nextImage = getRandomImageUrl();
-        while (activeImageUrls.includes(nextImage)) {
-          nextImage = getRandomImageUrl();
-        }
-        setActiveImageUrls([nextImage, ...activeImageUrls]);
-        swiper.slideTo(swiper.activeIndex + 1, 0);
-      }
-      if (swiper.activeIndex >= activeImageUrls.length - 3) {
-        let nextImage = getRandomImageUrl();
-        while (activeImageUrls.includes(nextImage)) {
-          nextImage = getRandomImageUrl();
-        }
-        setActiveImageUrls([...activeImageUrls, nextImage]);
-      }
-    }
-  };
+  // const onSlideChange = () => {
+  //   const swiper = swiperRef.current;
+  //   if (swiper) {
+  //     if (swiper.activeIndex <= 2) {
+  //       let nextImage = getRandomImageUrl();
+  //       while (activeImageUrls.includes(nextImage)) {
+  //         nextImage = getRandomImageUrl();
+  //       }
+  //       setActiveImageUrls([nextImage, ...activeImageUrls]);
+  //       swiper.slideTo(swiper.activeIndex + 1, 0);
+  //     }
+  //     if (swiper.activeIndex >= activeImageUrls.length - 3) {
+  //       let nextImage = getRandomImageUrl();
+  //       while (activeImageUrls.includes(nextImage)) {
+  //         nextImage = getRandomImageUrl();
+  //       }
+  //       setActiveImageUrls([...activeImageUrls, nextImage]);
+  //     }
+  //   }
+  // };
 
   useEffect(() => {
     const handleRightArrowPress = (e: KeyboardEvent) => {
@@ -84,7 +84,7 @@ const Page: NextPage<StaticProps> = ({ imageUrls }) => {
         <h1 className="p-4 text-center text-4xl text-neutral-100">
           For Sara ❤️
         </h1>
-        <Swiper
+        {/* <Swiper
           initialSlide={3}
           onSlideChangeTransitionEnd={onSlideChange}
           onSwiper={(swiper) => {
@@ -101,7 +101,7 @@ const Page: NextPage<StaticProps> = ({ imageUrls }) => {
               />
             </SwiperSlide>
           ))}
-        </Swiper>
+        </Swiper> */}
       </div>
     </>
   );
