94c5e76f18930894c6dd7f8a26285cc5dd38515f
Youssef Moussaoui <yms@outlook.com>
2024-01-23 18:31:44 -0800

    Update

--- src/lib/admin.ts
+++ src/lib/admin.ts
@@ -56,8 +56,8 @@ const ACTIONS: Record<string, (output: any[]) => Promise<void>> = {
     await POSTGRES_CACHE.delete(setCode);
     output.push(`${setCode.toUpperCase()} cache deleted`);
   },
-  "delete-ktk-cache": async (output) => {
-    const setCode = MagicSet.KHANS_OF_TARKIR.code;
+  "delete-lci-cache": async (output) => {
+    const setCode = MagicSet.CAVERNS_OF_IXALAN.code;
     await POSTGRES_CACHE.delete(setCode);
     output.push(`${setCode.toUpperCase()} cache deleted!`);
   },
